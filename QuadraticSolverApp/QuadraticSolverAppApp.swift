fcfa8b611138522c97f5a05fc761cadf379ae1a2
Janith <janith.mitkln@gmail.com>
2023-02-24 09:57:27 +0530

    Initial Commit

---
+++ QuadraticSolverApp/QuadraticSolverAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuadraticSolverAppApp.swift
+//  QuadraticSolverApp
+//
+//  Created by Janith Vidanagamage on 2023-02-24.
+//
+
+import SwiftUI
+
+@main
+struct QuadraticSolverAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
